5df990bde983bfbbc04f74f8631b19a0a9f500c1
yochien <ysam12345@gmail.com>
2019-05-31 09:45:47 +0800

    remove custom mpu6050 code

--- light_saber/light_saber.ino
+++ light_saber/light_saber.ino
@@ -1,11 +1,6 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
 #include "Timer.h"
 
 #define LED_0 3
@@ -29,29 +24,6 @@
 SoftwareSerial mySoftwareSerial(DFPlayer_RX, DFPlayer_TX); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
-//MPU 6050
-MPU6050 mpu;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars                                                                                                                                                                                                                                                                                                                                                                                                       
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-// packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-
 //LED state
 int led_light[6] = { 255 ,255 ,255 ,255 ,255 ,255 }; // 
 volatile int state = STATE_TURNED_OFF;
@@ -60,53 +32,10 @@ volatile int state = STATE_TURNED_OFF;
 long debouncing_time = 100; //Debouncing Time in Milliseconds
 volatile unsigned long last_micros;
 
-volatile bool mpuInterrupt = true;
-void dmpDataReady() {
-  mpuInterrupt = true;
-  //detachInterrupt(0);
-}
+
 
 void setup() {
   // put your setup code here, to run once:
-  
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-      Wire.begin();
-      Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-      Fastwire::setup(400, true);
-  #endif
-
-  Serial.begin(115200);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
-  
-  // initialize device
-  Serial.println(F("Initializing I2C devices..."));
-  mpu.initialize();
-  
-  // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // 載入與設定 DMP
-  Serial.println("Initializing DMP...\n");
-  devStatus = mpu.dmpInitialize();
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-  if (devStatus == 0) {
-    mpu.setDMPEnabled(true);
-    attachInterrupt(0, dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-    dmpReady = true;
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    Serial.println("DMP failed, code: " + devStatus);
-  }
   
   //init DFPlayer 
   mySoftwareSerial.begin(9600);
@@ -125,33 +54,6 @@ void setup() {
 void loop() {
   
   // put your main code here, to run repeatedl
-    mpuIntStatus = mpu.getIntStatus();
-    fifoCount = mpu.getFIFOCount();
-    if (fifoCount == 1024) {
-      mpu.resetFIFO();
-      Serial.println("FIFO overflow!");
-    } else if (fifoCount >= packetSize) {
-      mpu.getFIFOBytes(fifoBuffer, packetSize);
-      Serial.println("getFIFOBytes");
-      Serial.println(fifoCount);
-      Serial.println(packetSize);
-      //fifoCount -= packetSize;
-      
-
-      // 實際的加速度（去除重力）
-      mpu.dmpGetQuaternion(&q, fifoBuffer);
-      mpu.dmpGetAccel(&aa, fifoBuffer);
-      mpu.dmpGetGravity(&gravity, &q);
-      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-      printf("areal %6d %6d %6d    ", aaReal.x, aaReal.y, aaReal.z);
-
-      /*
-      mpu.dmpGetQuaternion(&q, fifoBuffer);
-      mpu.dmpGetGravity(&gravity, &q);
-      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);*/
-    } else {
-      Serial.println("dmp is not Ready");
-    }
   
   
   switch(state){
